be1d727e700775113a18321d632438fb56802efa
Jayjeet Chakraborty <jc.github@rediffmail.com>
2019-10-09 17:03:00 +0530

    Update ProfileAdd.js

--- assets/js/ProfileAdd.js
+++ assets/js/ProfileAdd.js
@@ -1,4 +1,4 @@
-$.getJSON('https://raw.githubusercontent.com/lugnitdgp/Hactoberfest-Hack-Day-2019/dev/data.json', function(data) {
+$.getJSON('https://raw.githubusercontent.com/lugnitdgp/Hactoberfest-Hack-Day-2019/master/data.json', function(data) {
     // console.log(data); // this will show the info it in firebug console
     var profiles = data.profiles;
     var cardParent = document.getElementById('profile-cards');
