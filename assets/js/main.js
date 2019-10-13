4d54bdce237d5662fc039616ba5ec96f565dcf79
Subashree Mahendran <subashreemsk@gmail.com>
2019-10-13 11:53:37 +0530

    offset value

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,12 +27,12 @@ var x = setInterval(function () {
         clearInterval(x);
         document.getElementById("demo").innerHTML = "EXPIRED";
     }
-}, 1000);
+}, 5000);
 
 
 $(function () {
     $('a[href*=#]').on('click', function (e) {
         e.preventDefault();
-        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 400, 'linear');
     });
 });
