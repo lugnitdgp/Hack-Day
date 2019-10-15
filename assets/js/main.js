0f272f64ff1c07234096e6d5e807bb2a3d91ed7b
Jamal Yarali <jamaldanlod@gmail.com>
2019-10-16 00:18:53 +0330

    fixed small bug on scroll to div function in main.js (#105)

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@ var x = setInterval(function () {
 
 
 $(function () {
-    $('a[href*=#]').on('click', function (e) {
+    $('a[href*="#"]').on('click', function (e) {
         e.preventDefault();
         $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
     });
