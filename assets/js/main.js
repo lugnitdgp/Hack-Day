9526b96391d17b3cacd9d2e96c24976cd8d4c57e
Proma Roy <promaroy20@gmail.com>
2019-10-16 18:43:42 +0530

    fixed the date and time

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 //Countdown-->
 
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 19, 2019 15:37:25").getTime();
+var countDownDate = new Date("Oct 17, 2019 17:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
