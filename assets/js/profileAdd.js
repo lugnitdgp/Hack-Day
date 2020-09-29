cafa66d948cd7c7abeda11d73943f84950d3708a
Abha Jha <jhaabha08@gmail.com>
2020-09-29 20:46:46 +0530

    [ADD] linter

--- assets/js/profileAdd.js
+++ assets/js/profileAdd.js
@@ -1,57 +1,55 @@
-$.getJSON('https://raw.githubusercontent.com/lugnitdgp/Hack-Day/2020/data.json', function(data) {
-    // console.log(data); // this will show the info it in firebug console
-    var profileKeys = ["handle", "image_link", "message"];
-    /**
-     * Check if a profile has handle image_link and message properties
-     */
-    var isProfileValid = function(profile) {
-      return profileKeys.every(k => k in profile)
-    };
+$.getJSON('https://raw.githubusercontent.com/lugnitdgp/Hack-Day/2020/data.json', (data) => {
+  // console.log(data); // this will show the info it in firebug console
+  let profileKeys = ['handle', 'image_link', 'message'];
+  /**
+   * Check if a profile has handle image_link and message properties
+   */
+  let isProfileValid = (profile) => (profileKeys.every((k) => k in profile));
 
-    /**
-     * Given an array of profiles, keep only one for handle (handle is the id of the profile)
-     */
-    var getUniqueProfiles = function(profiles) {
-      return Array.from(new Set(profiles.map(p => p.handle))).map(id => {
-        var profile = {}
-        profileKeys.forEach(k => profile[k] = profiles.find(p => p.handle === id)[k])
-        return profile;
-      })
-    }
+  /**
+   * Given an array of profiles, keep only one for handle (handle is the id of the profile)
+   */
+  let getUniqueProfiles = (profiles) => (
+    Array.from(new Set(profiles.map((p) => p.handle))).map((id) => {
+      let profile = {};
+      profileKeys.forEach((k) => {
+        profile[k] = profiles.find((p) => p.handle === id)[k];
+      });
+      return profile;
+    })
+  );
 
-    // get only unique and valid profiles
-    var profiles = getUniqueProfiles(data.profiles.filter(isProfileValid));
+  // get only unique and valid profiles
+  let profiles = getUniqueProfiles(data.profiles.filter(isProfileValid));
 
-    var cardParent = document.getElementById('profile-cards');
-    for (index = 0; index < profiles.length; index++){
-      var card = document.createElement('div');
-      card.classList.add('col-12');
-      card.classList.add('col-md-4');
-      card.classList.add('p-3');
+  let cardParent = document.getElementById('profile-cards');
+  for (let index = 0; index < profiles.length; index += 1) {
+    let card = document.createElement('div');
+    card.classList.add('col-12');
+    card.classList.add('col-md-4');
+    card.classList.add('p-3');
 
-      var profile = profiles[index];
-      card.innerHTML =
-            `<div class="card">
-              <div class="row no-gutters">
-                <div class="col-4">
-                  <img src="${profile['image_link']}" class="card-img" alt="${profile['handle']}">
-                </div>
-                <div class="col-8">
-                  <div class="card-body">
-                    <h6 class="card-title">${profile['handle']}</h6>
-                    <p><small>${profile['message']}</small></p>
-                  </div>
+    let profile = profiles[index];
+    card.innerHTML = `<div class='card'>
+            <div class='row no-gutters'>
+              <div class='col-4'>
+                <img src='${profile.image_link}' class='card-img' alt='${profile.handle}'>
+              </div>
+              <div class='col-8'>
+                <div class='card-body'>
+                  <h6 class='card-title'>${profile.handle}</h6>
+                  <p><small>${profile.message}</small></p>
                 </div>
               </div>
             </div>
-        `;
-      cardParent.appendChild(card);
-    }
-    if(!profiles.length)
-    {
-      var card = document.createElement('div');
-      card.classList.add('col-12');
-      card.innerHTML= "<h1>No ninjas encountered yet. Are you one? Feel free to add your card!</h1>";
-      cardParent.append(card);
-    }
-  });
+          </div>
+      `;
+    cardParent.appendChild(card);
+  }
+  if (!profiles.length) {
+    let card = document.createElement('div');
+    card.classList.add('col-12');
+    card.innerHTML = '<h1>No ninjas encountered yet. Are you one? Feel free to add your card!</h1>';
+    cardParent.append(card);
+  }
+});
